2932f549edae3d356ebbcb4c9d1f1b91a0708290
Can Ali Yarman <canaliyarman@gmail.com>
2022-02-19 18:34:34 +0300

    Added database creation, updated user creation, upload post, list posts

--- src.py
+++ src.py
@@ -1,13 +1,26 @@
 import os
-from re import U
+
+from sqlalchemy import null
 from app import app
 import urllib.request
+import sqlite3
 import boto3
 import json
-from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, send_file
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, send_file, jsonify, g, abort
 from werkzeug.utils import secure_filename
 
+#download, thumbnail, delete, select, sqlite, google vision ai, list images
+
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+conn = sqlite3.connect('pic_gallery.db')
+
+DATABASE = 'pic_gallery.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 f = open('creds.json')
 creds = json.load(f)
@@ -20,14 +33,51 @@ def upload_s3(filepath, username, filename):
 
     s3.upload_file(filepath, BUCKET, username + '/' + filename)
 
-
+def list_s3():
+    BUCKET = creds['s3_creds'][0]["BUCKET"]
+    ACCESS_KEY = creds['s3_creds'][0]["ACCESS_KEY"]
+    SECRET_KEY = creds['s3_creds'][0]["SECRET_KEY"]
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+    contents = []
+    ret_val = []
+    for item in s3.list_objects(Bucket=BUCKET)['Contents']:
+        contents.append(item)
+    return contents
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 	
+
+# Check user in database
+def auth_check(auth_query, password):
+    con = get_db()
+    cur = get_db().cursor()
+
+    cur.execute(auth_query)
+    rows = cur.fetchall()
+    if rows == []:
+        con.commit()
+        return 'Wrong username'
+    else:
+        if password != rows[0][2]:
+            con.commit()
+            return 'Wrong password'
+    con.commit()
+    return 1
+
+
+# Create user and write to database
 @app.route('/create_user', methods=['GET'])
 def create_user():
+    
     args = request.args
     name = args.get('name', type=str)
+    password = args.get('password', type=str)
+    #conn.execute("INSERT INTO USERS (NAME, PASSWORD ) VALUES (" + name + ", " + password ")")
+    query = 'INSERT INTO USERS(NAME,PASSWORD) VALUES("'  + name + '", "' + password + '")'
+    con = get_db()
+    cur = get_db().cursor()
+    cur.execute(query)
+    con.commit()
     path = './userdirs/' + name
     if not os.path.exists(path):
         os.makedirs(path)
@@ -38,11 +88,48 @@ def download_file(name):
     return send_from_directory(name)
 
 
+# Downloads all posts belonging to user, returns s3 elements of the user. Will add topic filtering in the future
+@app.route('/list_posts', methods=['GET'])
+def list_posts():
+    name = request.args.get('name', type=str)
+    password = request.args.get('password', type=str)
+    post_topic = request.args.get('topic', type=str)
+    print(post_topic)
+    posts = []
+    contents = list_s3()
+    for c in contents:
+        key = c['Key']
+        username = key.split('/')[0]
+        if username == name:
+            BUCKET = creds['s3_creds'][0]["BUCKET"]
+            ACCESS_KEY = creds['s3_creds'][0]["ACCESS_KEY"]
+            SECRET_KEY = creds['s3_creds'][0]["SECRET_KEY"]
+            s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+            with open('downloads/' + key.split('/')[1], 'wb') as f:
+                s3.download_fileobj(BUCKET, key, f)
+                posts.append(c)
+    return jsonify(posts)
+
+
+# Upload image to s3 and insert post to posts table
 @app.route('/upload_post', methods=['GET', 'POST'])
 def upload_file():
     name = request.args.get('name', type=str)
-    if not os.path.exists('./userdirs/' + name):
-        return 'sen kimsin'
+    password = request.args.get('password', type=str)
+    post_topic = request.args.get('topic', type=str)
+    if post_topic == null:
+        post_topic = ''
+#    if not os.path.exists('./userdirs/' + name):
+#        return 'sen kimsin'
+
+    auth_query = 'SELECT * FROM USERS WHERE NAME="' + name + '";'
+
+    res = auth_check(auth_query, password)
+    if res != 1:
+        abort(401)
+
+    con = get_db()
+    cur = get_db().cursor()
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -58,6 +145,9 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join('userdirs/' + name + '/', filename))
             upload_s3('userdirs/' + name + '/' + filename, name, filename)
+            post_query = 'INSERT INTO POSTS(NAME, USER_NAME, POST_TAG, S3_KEY) VALUES("'  + filename + '", "' + name + '", "' + str(post_topic) + '", "' + name + '/' + filename + '")'
+            cur.execute(post_query)
+            con.commit()
             return send_file('./userdirs/' + name + '/' + filename)
     return '''
     <!doctype html>
